fb82da0a6f4386ac84407d1b284792a583352c58
dvlp-sy <sy8389b@gmail.com>
2024-05-04 12:59:27 +0900

    [fix] fix test code

--- spring-study/src/test/java/net/springstudy/repository/MemoryMemberRepositoryTest.java
+++ spring-study/src/test/java/net/springstudy/repository/MemoryMemberRepositoryTest.java
@@ -4,19 +4,22 @@ import net.springstudy.domain.Member;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 public class MemoryMemberRepositoryTest
 {
-    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+    @Autowired MemberRepository memberRepository;
 
     @AfterEach
     public void afterEach()
     {
-        memoryMemberRepository.clearStore();
+        memberRepository.clearStore();
     }
 
     @Test
@@ -24,9 +27,9 @@ public class MemoryMemberRepositoryTest
     {
         Member member = new Member();
         member.setName("spring");
-        memoryMemberRepository.save(member);
+        memberRepository.save(member);
 
-        Member result = memoryMemberRepository.findById(member.getId()).get();
+        Member result = memberRepository.findById(member.getId()).get();
 
         Assertions.assertEquals(member, result);
     }
@@ -36,9 +39,9 @@ public class MemoryMemberRepositoryTest
     {
         Member member = new Member();
         member.setName("spring");
-        memoryMemberRepository.save(member);
+        memberRepository.save(member);
 
-        Member result = memoryMemberRepository.findByName("spring").get();
+        Member result = memberRepository.findByName("spring").get();
         assertThat(member).isEqualTo(result);
     }
 
@@ -47,13 +50,13 @@ public class MemoryMemberRepositoryTest
     {
         Member member1 = new Member();
         member1.setName("spring");
-        memoryMemberRepository.save(member1);
+        memberRepository.save(member1);
 
         Member member2 = new Member();
         member2.setName("spring");
-        memoryMemberRepository.save(member2);
+        memberRepository.save(member2);
 
-        List<Member> result = memoryMemberRepository.findAll();
+        List<Member> result = memberRepository.findAll();
 
         assertThat(result.size()).isEqualTo(2);
     }
--- spring-study/src/test/java/net/springstudy/service/MemberServiceTest.java
+++ spring-study/src/test/java/net/springstudy/service/MemberServiceTest.java
@@ -6,23 +6,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest
 class MemberServiceTest {
 
-    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
-    MemberService memberService = new MemberService(memoryMemberRepository);
-
-    @BeforeEach
-    public void beforeEach()
-    {
-        memoryMemberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memoryMemberRepository);
-    }
-
+    @Autowired MemoryMemberRepository memoryMemberRepository;
+    @Autowired MemberService memberService;
+    
     @AfterEach
     void afterEach()
     {
