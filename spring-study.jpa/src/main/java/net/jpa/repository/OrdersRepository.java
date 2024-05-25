a81cd2538293b1a88255aa7167a5d4cc77d6ef31
dvlp-sy <sy8389b@gmail.com>
2024-05-25 17:10:18 +0900

    [add] Add OrderSearch JPA Criteria Code

--- spring-study.jpa/src/main/java/net/jpa/repository/OrdersRepository.java
+++ spring-study.jpa/src/main/java/net/jpa/repository/OrdersRepository.java
@@ -1,9 +1,17 @@
 package net.jpa.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
+import net.jpa.domain.Member;
+import net.jpa.domain.OrderSearch;
 import net.jpa.domain.Orders;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,6 +25,29 @@ public class OrdersRepository {
 
     public Orders findOne(Long id) {
         return em.find(Orders.class, id);
+    }
+
+    public List<Orders> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Orders> cq = cb.createQuery(Orders.class);
+        Root<Orders> o = cq.from(Orders.class);
+        Join<Orders, Member> m = o.join("member", JoinType.INNER);
+        List<Predicate> criteria = new ArrayList<>();
+
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Orders> query = em.createQuery(cq).setMaxResults(1000); //최대 1000 건
+        return query.getResultList();
     }
 
 }
